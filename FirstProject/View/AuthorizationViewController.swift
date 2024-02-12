4b89fe259aad55180350a3a7629390134b1c0c36
tatina140810 <tatu140810tatu@gmail.com>
2024-02-13 00:15:02 +0600

    first commit

--- FirstProject/View/AuthorizationViewController.swift
+++ FirstProject/View/AuthorizationViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AuthorizationViewController.swift
 //  FirstProject
 //
 //  Created by Tatina Dzhakypbekova on 12/2/24.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class AuthorizationViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
-
